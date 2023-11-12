a785d5ae56f54148966ea29274807db0341f8936
Cottolo <rahmat9654@gmail.com>
2023-11-12 20:41:18 +0700

    first

---
+++ src/main/java/com/domain/wayslink/WayslinkApplication.java
@@ -0,0 +1,13 @@
+package com.domain.wayslink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayslinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayslinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domain/wayslink/WayslinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.wayslink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayslinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
